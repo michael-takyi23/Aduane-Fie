43ddcab048783bc39043704969f03e0b34371843
Michael Oppong Takyi <michaeltakyi11@gmail.com>
2024-07-26 13:21:28 +0000

    Initial commit, Add bookings models

---
+++ restaurant_booking/bookings/models.py
@@ -0,0 +1,31 @@
+# bookings/models.py
+from django.db import models
+from django.contrib.auth.models import User
+
+class Table(models.Model):
+    number = models.IntegerField(unique=True)
+    capacity = models.IntegerField()
+
+    def __str__(self):
+        return f"Table {self.number} (Capacity: {self.capacity})"
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+class Booking(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    table = models.ForeignKey(Table, on_delete=models.CASCADE)
+    date = models.DateField()
+    time = models.TimeField()
+    guests = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Booking for {self.user.username} on {self.date} at {self.time}"
+
+
